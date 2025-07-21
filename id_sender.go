019cc56b16c53b8fdc189ce0f36196c97ba28be8
huang <2302719556@qq.com>
2025-07-21 10:26:39 +0800

    2025/7/21

---
+++ id_sender.go
@@ -0,0 +1,20 @@
+package id_sender
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"id_sender/model"
+)
+
+func Sender(datasource string, biz_tag string) int64 {
+	conn := sqlx.NewMysql(datasource)
+	idSenderModel := model.NewIdSenderModel(conn)
+	err := idSenderModel.UpdateCurrentIdAutoStep(biz_tag)
+	if err != nil {
+		panic(err)
+	}
+	id, err := idSenderModel.FindOneByBizTag(biz_tag)
+	if err != nil {
+		panic(err)
+	}
+	return id.Id
+}
